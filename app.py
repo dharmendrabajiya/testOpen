1f97f74ef75030065f5df1c68c89382edba8870b
dharmendrabajiya <73024965+dharmendrabajiya@users.noreply.github.com>
2022-07-01 12:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  return {'msg': 'working'}
- 
+    return {'msg': 'working'}
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3089)
+    app.run(host='0.0.0.0', port=3089)
